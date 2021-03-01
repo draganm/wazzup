160123c94c2efcc72e1fa0f1e2c148007cf40511
Dragan Milic <dragan@netice9.com>
2021-03-01 23:02:52 +0000

    fix tailing same container multiple times, filter events only for pods

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"sync"
 
 	"github.com/draganm/wazzup/logwriter"
 	"github.com/pkg/errors"
@@ -47,6 +48,8 @@ func main() {
 				return errors.Wrap(err, "while creating k8s client")
 			}
 
+			pods := newPodsByName()
+
 			lines := make(chan string)
 
 			evtclient := kubeclient.CoreV1().Events(c.String("namespace"))
@@ -55,25 +58,25 @@ func main() {
 				return errors.Wrap(err, "while creating event watch")
 			}
 
-			go func() {
-				for e := range evtWatch.ResultChan() {
-					evt := e.Object.(*corev1.Event)
-					lines <- fmt.Sprintf("EVENT %s(%s): %s", evt.InvolvedObject.Kind, evt.ObjectMeta.Name, evt.Message)
-				}
-			}()
-
 			deploymentsClient := kubeclient.AppsV1().Deployments(c.String("namespace"))
-			podsClient := kubeclient.CoreV1().Pods(c.String("namespace"))
-
 			depl, err := deploymentsClient.Get(context.Background(), deploymentName, v1.GetOptions{})
 			if err != nil {
 				return errors.Wrap(err, "while getting deployment")
 			}
 
 			podSelector := depl.Spec.Selector
-			podSelector.Marshal()
 
-			fmt.Println("selector", v1.FormatLabelSelector(podSelector))
+			go func() {
+				for e := range evtWatch.ResultChan() {
+					evt := e.Object.(*corev1.Event)
+					if evt.InvolvedObject.Kind == "Pod" && pods.contains(evt.InvolvedObject.Name) {
+						lines <- fmt.Sprintf("EVENT %s(%s): %s", evt.InvolvedObject.Kind, evt.InvolvedObject.Name, evt.Message)
+					}
+
+				}
+			}()
+
+			podsClient := kubeclient.CoreV1().Pods(c.String("namespace"))
 
 			podWatch, err := podsClient.Watch(context.Background(), v1.ListOptions{
 				LabelSelector: v1.FormatLabelSelector(podSelector),
@@ -87,93 +90,96 @@ func main() {
 
 			go func() {
 
-				podStates := map[string]*corev1.Pod{}
-
+			podWatchLoop:
 				for evt := range podWatch.ResultChan() {
 					pod, castOk := evt.Object.(*corev1.Pod)
-					if castOk {
+					if !castOk {
+						continue podWatchLoop
+					}
 
-						podName := pod.ObjectMeta.Name
-						switch evt.Type {
-						case watch.Added, watch.Modified:
-							if evt.Type == watch.Added {
-								lines <- fmt.Sprintf("POD %s created", podName)
-							}
-							old, hasOld := podStates[podName]
-							if !hasOld {
-								statuses := append(pod.Status.InitContainerStatuses, pod.Status.ContainerStatuses...)
-								for _, c := range statuses {
+					podName := pod.ObjectMeta.Name
+					switch evt.Type {
+					case watch.Added:
+						pods.set(podName, pod)
+						if evt.Type == watch.Added {
+							lines <- fmt.Sprintf("POD %s created", podName)
+						}
+						statuses := append(pod.Status.InitContainerStatuses, pod.Status.ContainerStatuses...)
+						for _, c := range statuses {
 
-									shouldTailLogs := c.State.Waiting == nil
-									if !shouldTailLogs {
-										continue
-									}
-									containerName := c.Name
-									tailLines := int64(30)
-									containerLogs := podsClient.GetLogs(podName, &corev1.PodLogOptions{Container: containerName, Follow: true, TailLines: &tailLines})
-									go func() {
-										st, err := containerLogs.Stream(context.Background())
-										if err != nil {
-											lines <- fmt.Sprintf("error while getting logs for container %s of pod %s: %s", containerName, podName, err.Error())
-											return
-										}
-										defer st.Close()
-										lw := logwriter.New(func(lns []string) error {
-											for _, l := range lns {
-												lines <- fmt.Sprintf("LOG(%s/%s): %s", podName, containerName, l)
-											}
-											return nil
-										})
-										_, err = io.Copy(lw, st)
-										if err != nil {
-											lines <- fmt.Sprintf("error while reading/writing logs for container %s of pod %s: %s", containerName, podName, err.Error())
-										}
-										lines <- fmt.Sprintf("END OF LOG: container %s in pod %s terminated", podName, containerName)
-									}()
-
-								}
+							shouldTailLogs := c.State.Waiting == nil
+							if !shouldTailLogs {
 								continue
 							}
-
-							oldStatuses := append(old.Status.InitContainerStatuses, old.Status.ContainerStatuses...)
-							newStatuses := append(pod.Status.InitContainerStatuses, pod.Status.ContainerStatuses...)
-
-							for i, oldStatus := range oldStatuses {
-								newStatus := newStatuses[i]
-
-								if newStatus.State.Waiting == nil && oldStatus.State.Waiting != nil {
-									containerName := newStatus.Name
-									tailLines := int64(30)
-									containerLogs := podsClient.GetLogs(podName, &corev1.PodLogOptions{Container: containerName, Follow: true, TailLines: &tailLines})
-									go func() {
-										st, err := containerLogs.Stream(context.Background())
-										if err != nil {
-											lines <- fmt.Sprintf("error while getting logs for container %s of pod %s: %s", containerName, podName, err.Error())
-											return
-										}
-										defer st.Close()
-										lw := logwriter.New(func(lns []string) error {
-											for _, l := range lns {
-												lines <- fmt.Sprintf("LOG(%s/%s): %s", podName, containerName, l)
-											}
-											return nil
-										})
-										_, err = io.Copy(lw, st)
-										if err != nil {
-											lines <- fmt.Sprintf("error while reading/writing logs for container %s of pod %s: %s", containerName, podName, err.Error())
-										}
-										lines <- fmt.Sprintf("LOG(%s/%s): %s", podName, containerName, "END OF LOG")
-									}()
+							containerName := c.Name
+							tailLines := int64(30)
+							containerLogs := podsClient.GetLogs(podName, &corev1.PodLogOptions{Container: containerName, Follow: true, TailLines: &tailLines})
+							go func() {
+								st, err := containerLogs.Stream(context.Background())
+								if err != nil {
+									lines <- fmt.Sprintf("error while getting logs for container %s of pod %s: %s", containerName, podName, err.Error())
+									return
 								}
+								defer st.Close()
+								lw := logwriter.New(func(lns []string) error {
+									for _, l := range lns {
+										lines <- fmt.Sprintf("LOG(%s/%s): %s", podName, containerName, l)
+									}
+									return nil
+								})
+								_, err = io.Copy(lw, st)
+								if err != nil {
+									lines <- fmt.Sprintf("error while reading/writing logs for container %s of pod %s: %s", containerName, podName, err.Error())
+								}
+								lines <- fmt.Sprintf("END OF LOG: container %s in pod %s terminated", podName, containerName)
+							}()
+
+						}
+						continue podWatchLoop
+					case watch.Modified:
+						if !pods.contains(podName) {
+							continue podWatchLoop
+						}
+						old := pods.get(podName)
+						pods.set(podName, pod)
+
+						oldStatuses := append(old.Status.InitContainerStatuses, old.Status.ContainerStatuses...)
+						newStatuses := append(pod.Status.InitContainerStatuses, pod.Status.ContainerStatuses...)
+
+						for i, oldStatus := range oldStatuses {
+							newStatus := newStatuses[i]
+
+							if newStatus.State.Waiting == nil && oldStatus.State.Waiting != nil {
+								containerName := newStatus.Name
+								tailLines := int64(30)
+								containerLogs := podsClient.GetLogs(podName, &corev1.PodLogOptions{Container: containerName, Follow: true, TailLines: &tailLines})
+								go func() {
+									st, err := containerLogs.Stream(context.Background())
+									if err != nil {
+										lines <- fmt.Sprintf("error while getting logs for container %s of pod %s: %s", containerName, podName, err.Error())
+										return
+									}
+									defer st.Close()
+									lw := logwriter.New(func(lns []string) error {
+										for _, l := range lns {
+											lines <- fmt.Sprintf("LOG(%s/%s): %s", podName, containerName, l)
+										}
+										return nil
+									})
+									_, err = io.Copy(lw, st)
+									if err != nil {
+										lines <- fmt.Sprintf("error while reading/writing logs for container %s of pod %s: %s", containerName, podName, err.Error())
+									}
+									lines <- fmt.Sprintf("LOG(%s/%s): %s", podName, containerName, "END OF LOG")
+								}()
 							}
-
-							podStates[podName] = pod
-
-						case watch.Deleted:
-							lines <- fmt.Sprintf("POD %s deleted", podName)
 						}
 
+					case watch.Deleted:
+						pods.delete(podName)
+						lines <- fmt.Sprintf("POD %s deleted", podName)
 					}
+
 				}
 			}()
 
@@ -198,4 +204,41 @@ func main() {
 		},
 	}
 	a.RunAndExitOnError()
+}
+
+type podsByName struct {
+	pods map[string]*corev1.Pod
+	mu   *sync.Mutex
+}
+
+func newPodsByName() *podsByName {
+	return &podsByName{
+		pods: map[string]*corev1.Pod{},
+		mu:   new(sync.Mutex),
+	}
+}
+
+func (p *podsByName) contains(name string) bool {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	_, found := p.pods[name]
+	return found
+}
+
+func (p *podsByName) get(name string) *corev1.Pod {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	return p.pods[name]
+}
+
+func (p *podsByName) set(name string, pod *corev1.Pod) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.pods[name] = pod
+}
+
+func (p *podsByName) delete(name string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	delete(p.pods, name)
 }
